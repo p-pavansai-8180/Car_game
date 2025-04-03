639b59aeb8b83c95a6af40d40e33b8643fb3d0b4
p_pavan_sai <potturpavansai.2005@gmail.com>
2025-04-03 13:35:48 +0530

    Add files via upload

---
+++ Car_game.py
@@ -0,0 +1,11 @@
+name=input('enter the word:: ')
+find='help'
+
+if name==find:
+    print('start - to start the car')
+    print('stop - to syop the car')
+    print('quit - to exit')
+elif name=='start':
+    print('car started')
+elif name=='stop':
+    print('car stopped')
